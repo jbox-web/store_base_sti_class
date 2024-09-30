21b3ab36f1a36ec97b0447bd9d48c77ea1505d1a
Nicolas Rodriguez <nico@nicoladmin.fr>
2024-09-30 02:26:27 +0200

    Coding style

--- lib/store_base_sti_class.rb
+++ lib/store_base_sti_class.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
+# require external dependencies
 require 'active_record'
 
-require "store_base_sti_class_for_#{ActiveRecord::VERSION::MAJOR}_#{ActiveRecord::VERSION::MINOR}"
+# require internal dependencies
+require_relative "store_base_sti_class_for_#{ActiveRecord::VERSION::MAJOR}_#{ActiveRecord::VERSION::MINOR}"
 
 module StoreBaseSTIClass
 end
